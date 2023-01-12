c74126e24250af25e5a121210a3cf3f4ec27cc18
Tiala Young <tialayoung@gmail.com>
2023-01-11 17:59:31 -0700

    Add explanation of index position

--- arrays.js
+++ arrays.js
@@ -12,3 +12,12 @@ console.log(milesDriven.slice(1));
 // This shift method will remove the first element from 'answers' array, returning 4 elements: [false, false, true, true]
 answers.shift();
 console.log(answers);
+
+// Index position is the relative place of each element within an Array
+// and is used when accessing elements using bracket notation.
+// In Javascript, and most other programming languages,
+// the first element in an array has Index position 0.
+// Each subsequent element increases by 1 Index position.
+// For example, an Array of 10 elements would have Index positions 0 thru 9.
+// In my variable soupVegetables, Index position 3 is declared at "peas".
+// By the end of this code, my variable soupVegetables has max Index position of 4.
